1bf1cfda49141c4f915cf6935c691368b7a2d379
Will S <wsi@huhu.ai>
2023-09-17 22:33:36 -0700

    fix: change brush color

--- Sources/BrightroomUI/Shared/Components/Drawing/BlurryMaskingView.swift
+++ Sources/BrightroomUI/Shared/Components/Drawing/BlurryMaskingView.swift
@@ -234,7 +234,7 @@ public final class BlurryMaskingView: PixelEditorCodeBasedView, UIScrollViewDele
     
     defaultAppearance: do {
       setLoadingOverlay(factory: {
-        LoadingBlurryOverlayView(effect: UIBlurEffect(style: .dark), activityIndicatorStyle: .whiteLarge)
+          LoadingBlurryOverlayView(effect: UIBlurEffect(style: .prominent), activityIndicatorStyle: .medium)
       })
     }
   }
