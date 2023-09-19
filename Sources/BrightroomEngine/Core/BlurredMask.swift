2ad355b821bcd1e1d9b054e7550fc1a1363a6c0e
Will S <wsi@huhu.ai>
2023-09-19 08:57:03 -0700

    convert it back

--- Sources/BrightroomEngine/Core/BlurredMask.swift
+++ Sources/BrightroomEngine/Core/BlurredMask.swift
@@ -127,8 +127,7 @@ public struct BlurredMask: GraphicsDrawing {
 
     let _radius = radius(image.extent) * value / max
     
-//    let grayColor = CIColor(red: 0.5, green: 0.5, blue: 0.5)  // This represents a middle gray. Adjust as needed.
-      let grayColor = CIColor.white
+    let grayColor = CIColor(red: 0.5, green: 0.5, blue: 0.5)  // This represents a middle gray. Adjust as needed.
       
     let outputImage = image
       .clamped(to: image.extent)
