c7ad300ecb2a17ab322a085cfa6e2986375289f9
Will S <wsi@huhu.ai>
2023-12-05 07:30:00 -0800

    change it back

--- Sources/BrightroomUI/Shared/Components/Drawing/BlurryMaskingView.swift
+++ Sources/BrightroomUI/Shared/Components/Drawing/BlurryMaskingView.swift
@@ -35,7 +35,7 @@ public final class BlurryMaskingView: PixelEditorCodeBasedView, UIScrollViewDele
     
     fileprivate(set) var proposedCrop: EditingCrop?
     
-    public var brushSize: CanvasView.BrushSize = .point(20)
+    fileprivate(set) var brushSize: CanvasView.BrushSize = .point(20)
     
     fileprivate let contentInset: UIEdgeInsets = .zero
     
