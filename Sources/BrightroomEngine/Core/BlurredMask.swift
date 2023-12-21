2b9b77cfc76021cea24bfb6cda43927ac818c02e
Will S <wsi@huhu.ai>
2023-12-21 00:23:51 -0800

    update

--- Sources/BrightroomEngine/Core/BlurredMask.swift
+++ Sources/BrightroomEngine/Core/BlurredMask.swift
@@ -149,7 +149,7 @@ public struct BlurredMask: GraphicsDrawing {
     
     public static func fakeMask(image: CIImage) -> CIImage? {
         
-        return generateGrayImage(width: image.extent.width, height: image.extent.height, grayValue: 0.2)
+        return generateGrayImage(width: image.extent.width, height: image.extent.height, grayValue: 0.8)
     }
     
     
